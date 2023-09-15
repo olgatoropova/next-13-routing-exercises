27935d0607f8c3c39acefac61f5868d6a0dc1048
Olga Toropova <fluctuacia@gmail.com>
2023-09-15 14:27:56 -0700

    Dynamic screen saver color

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import Link from 'next/link';
 
-function ScreenSaverExercise() {
-  return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
-    </main>
-  );
+function ScreenSaverSelection() {
+	return (
+		<main className="screen-saver-wrapper">
+			<h1>Choose your color:</h1>
+			<ul>
+				<li>
+					<Link href="/exercises/01-screensaver/yellow">yellow</Link>
+				</li>
+				<li>
+					<Link href="/exercises/01-screensaver/purple">purple</Link>
+				</li>
+				<li>
+					<Link href="/exercises/01-screensaver/pink">pink</Link>
+				</li>
+				<li>
+					<Link href="/exercises/01-screensaver/white">white</Link>
+				</li>
+			</ul>
+		</main>
+	);
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverSelection;
